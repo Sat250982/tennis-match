03858c454f305eadbd7346380bcb6a51e820202d
Sathwick Mahadeva <sathwickm@gmail.com>
2026-07-03 19:09:09 +0530

    rename variables

--- tennis_match.py
+++ tennis_match.py
@@ -13,26 +13,42 @@ def game_won(winner_points, loser_points):
      else:
          return False
 
+def match_won(winner_games_count, loser_games_count):
+    if ( winner_games_count >=6 and (winner_games_count - loser_games_count >=2)):
+        return True
+    else:
+        return False
+
 
 def find_winner(tennis_points):
-    game_points = {"A": 0, "B": 0}
-    game = {"A": 0, "B": 0}
+    game_points = {"A": 0, "B": 0}             # 15, 30, 40 etc
+    game_won_count = {"A": 0, "B": 0}                    # 1,2,3, 4, 5, 6
 
     for point_winner in tennis_points:
         point_loser = opponent(point_winner)
         game_points[point_winner] += 1
 
         if game_won(game_points[point_winner],game_points[point_loser]):
-            game[point_winner] += 1
+            game_won_count[point_winner] += 1
             game_points = { "A": 0, "B": 0 }
 
-
-    return game
-
-
+            if match_won(game_won_count[point_winner],game_won_count[point_loser]):
+                 return point_winner, game_won_count
 
 
+    return
 
-tennis_points = [ "A", "B", "B", "B","A", "B"]
-result = find_winner(tennis_points)
-print (result )
+
+
+
+
+# all won by b
+tennis_points = [ "A", "B", "B", "B","A", "B", "A", "B", "B", "B","A", "B", "A", "B", "B", "B","A", "B", "A", "B", "B", "B","A", "B", "A", "B", "B", "B","A", "B", "A", "B", "B", "B","A", "B"]
+
+tennis_points = [ "A", "B", "B", "B","A", "B", "A", "B", "B", "B","A", "B", "A", "B", "B", "B","A", "B", "A", "B", "B", "B","A", "B", "A", "B", "B", "B","A", "B", "A", "B", "B", "B","A", "B"]
+
+point_winner, game_won_count = find_winner(tennis_points)
+
+
+print (point_winner)
+print (game_won_count)
